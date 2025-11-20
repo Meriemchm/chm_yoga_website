ce799a94401cd2dca925f9ec624d52cf65d03dec
Meriemchm <chamimeriem2302@gmail.com>
2025-11-20 18:45:32 +0100

    Update:  banner component change text styles

--- components/banner/banner.tsx
+++ components/banner/banner.tsx
@@ -11,8 +11,8 @@ export const Banner = () => {
     >
       <div ref={fadeUp} className="flex flex-col justify-center items-center">
         
-        <p className="text-4xl md:text-5xl font-semibold text-black max-w-3xl">
-          You Can always control what goes inside you
+        <p className="text-4xl md:text-5xl font-light text-black max-w-2xl capitalize">
+          You Can always <span className="cormorant">control</span> what goes inside you
         </p>
 
         <Button className="mt-6 px-10">Get Started</Button>
